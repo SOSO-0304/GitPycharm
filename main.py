b83a48616dfa9128bef087e6f7d2c4af6283da4c
So Hyun Jee <77823902+SOSO-0304@users.noreply.github.com>
2025-04-11 20:52:11 +0900

    Update on Github

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
-# 샘플 Python 스크립트입니다.
+#
+#
+#
+print('Hello World')
 
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+# adding new item in hotfix
+print('Hello World 2')
 
+# adding new item in master branch
+print('Hello World 3')
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+# adding new item in hotfix
+print('Hello World 4')
 
+# adding new item in master branch
+print('Hello World 5')
 
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print('Hello World')
-
-    # adding new item in hotfix
-    print('Hello World 2')
-
-    # adding new item in master branch
-    print('Hello World 3')
-
-    # adding new item in hotfix
-    print('Hello World 4')
-
-    # adding new item in master branch
-    print('Hello World 5')
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+# Updates on mainbranch of GitHub
+print("Hello World Updates on GitHub")
